f43dc51a92ba9db0127639be69b1f5a6fefb404b
Ross Meyer <metsrm@yahoo.com>
2020-05-25 21:55:07 -0400

    Added

--- School_05252020/Program.cs
+++ School_05252020/Program.cs
@@ -8,6 +8,10 @@ namespace School_05252020
         {
             Console.WriteLine("Learning to use Git!");
             Console.WriteLine("The GUI is better than Console");
+
+            Console.WriteLine("The boss says no more poems (unless it's about roses)");
+
+            Console.WriteLine("I wish issues");
         }
     }
 }
