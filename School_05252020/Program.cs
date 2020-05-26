e5c85059156725c0539a8459d9b9beaa5d9eddcf
Ross Meyer <metsrm@yahoo.com>
2020-05-25 21:20:43 -0400

    Lines added

--- School_05252020/Program.cs
+++ School_05252020/Program.cs
@@ -7,6 +7,8 @@ namespace School_05252020
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Learning to use Git!");
+            Console.WriteLine("Added third line!");
         }
     }
 }
